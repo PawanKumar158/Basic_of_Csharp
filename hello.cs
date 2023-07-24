1f2889692caea459736d14534727b0325355fffc
Pawan Kumar Gautam <90699283+PawanKumar158@users.noreply.github.com>
2023-07-24 22:44:20 +0530

    Add files via upload
    It is hello world program

---
+++ hello.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace Dcoder
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       //Your code goes here
+       Console.WriteLine("Hello, batch mate!");
+       Console.WriteLine("\nHow was a day ?");
+       Console.Write("I AM A PROGRAMMER to write a code to myself in new learning technology");
+      }
+   }
+ }
